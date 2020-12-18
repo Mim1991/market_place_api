d820e03205200677ca108d9ece96f435cb65b702
Meehir Patel <meehir_patel@hotmail.co.uk>
2020-12-18 11:58:29 +0000

    added actions for index and create

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -5,8 +5,8 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     @product = products(:one)
   end
 
-  test 'shoould show products' do
-    get api_v1_product_url(), as: :json
+  test 'should show products' do
+    get api_v1_products_url(), as: :json
     assert_response :success
   end
 
@@ -16,5 +16,24 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
 
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
+  end
+
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post api_v1_products_url,
+        params: { product:  {title: @product.title, price: @product.price, published: @product.published  } },
+        headers: {  Authorization: JsonWebToken.encode(user_id: @product.user_id)  },
+        as: :json
+    end
+    assert_response :created
+  end
+
+  test 'should forbid create product' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+      params: { product: { title: @product.title, price: @product.price, published: @product.published  } },
+      as: :json
+    end
+    assert_response :forbidden
   end
 end
